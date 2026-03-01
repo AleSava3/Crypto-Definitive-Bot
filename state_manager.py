084fe3f89f411d43f6dac70fddf6bee3b00ccfcf
AleSava3 <alessiojuve03@gmail.com>
2026-03-01 23:23:44 +0100

    Create state_manager.py

---
+++ state_manager.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+class State:
+    def __init__(self):
+        self.today = date.today()
+        self.signals_today = 0
+        self.high_today = 0
+        self.consecutive_high_losses = 0
+
+    def reset_if_new_day(self):
+        if date.today() != self.today:
+            self.today = date.today()
+            self.signals_today = 0
+            self.high_today = 0
+            self.consecutive_high_losses = 0
+
+state = State()
